71659db4dedf01776053c292090cfe6375e14dd1
amith59 <er.devopsamit@gmail.com>
2024-12-09 22:34:00 +0530

    Update array.js

--- array.js
+++ array.js
@@ -8,3 +8,4 @@ const myList = myArray.map((item) => <p>{item}</p>)
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(myList);
+#update for web hooks configurationa
